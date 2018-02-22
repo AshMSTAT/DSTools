74eba9f3232551650ffd752f3889f62563ece08f
Ash <ash.row@gmail.com>
2018-02-21 20:13:54 -0700

    'initializing'

---
+++ R/SamplingDist.R
@@ -0,0 +1,20 @@
+#' @title Genereates
+#' @param  samples Specify how many samples
+#'
+#' @export
+
+
+
+
+
+SamplingDist <- function(samples=50, sample_size=1000, mean = 0, sd = 1){
+
+
+  sampleMeans <- rep(NA,samples)
+  for (i in 1:samples){
+    x <- rnorm(sample_size, mean = mean, sd = sd)
+    sampleMeans[i] <- mean(x)
+  }
+
+  return(sampleMeans)
+}
